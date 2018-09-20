ecbfdae89c6b62cd3be8d1dbc684e2f5ca866637
Jessica Gladstone <jcg321@comcast.net>
2018-09-20 20:10:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function removeFromCart(item) {
   
 function placeOrder(ccNum) {
 	if (arguments.length === 0) {
-		console.log("Sorry, we don't have a credit card on file for you.");
+  return "Sorry, we don't have a credit card on file for you.";
 		
 	} else if (ccNum>0 ) {
 		for (let i = 0; i < cart.length; i++) {
